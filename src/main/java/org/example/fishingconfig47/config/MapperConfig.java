7227683fce05b2452fe147fd7e7eb5049913fd80
frykt <coch.frykt@gmail.com>
2024-08-22 13:32:06 +0300

    add: mapper configuration

--- src/main/java/org/example/fishingconfig47/config/MapperConfig.java
+++ src/main/java/org/example/fishingconfig47/config/MapperConfig.java
@@ -1,13 +1,39 @@
 package org.example.fishingconfig47.config;
 
+import org.example.fishingconfig47.dto.FishingKitDto;
+import org.example.fishingconfig47.entities.BaseEntity;
+import org.example.fishingconfig47.entities.FishingKit;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfig {
+    private ModelMapper mapper;
+
+    public MapperConfig() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        initializeModelMapper(modelMapper);
+
+        mapper = modelMapper;
+    }
+
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        return mapper;
+    }
+
+    private void initializeModelMapper(ModelMapper modelMapper) {
+        modelMapper.typeMap(FishingKit.class, FishingKitDto.class).addMappings(mapper -> {
+            mapper.map(BaseEntity::getId, FishingKitDto::setId);
+            mapper.map(fishingKit -> fishingKit.getRod().getId(), FishingKitDto::setRodId);
+            mapper.map(fishingKit -> fishingKit.getReel().getId(), FishingKitDto::setReelId);
+            mapper.map(fishingKit -> fishingKit.getLine().getId(), FishingKitDto::setLineId);
+            mapper.map(fishingKit -> fishingKit.getLure().getId(), FishingKitDto::setLureId);
+            mapper.map(FishingKit::getName, FishingKitDto::setName);
+            mapper.map(FishingKit::getFishWeight, FishingKitDto::setFishWeight);
+            mapper.map(FishingKit::getFishCount, FishingKitDto::setFishCount);
+        });
     }
 }
