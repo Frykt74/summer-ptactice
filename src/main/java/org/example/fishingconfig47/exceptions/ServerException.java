3ec18a9bc2aded238806a874f74aea39be4957c9
frykt <coch.frykt@gmail.com>
2024-08-26 19:38:07 +0300

    add: Server Exception

---
+++ src/main/java/org/example/fishingconfig47/exceptions/ServerException.java
@@ -0,0 +1,7 @@
+package org.example.fishingconfig47.exceptions;
+
+public class ServerException extends RuntimeException {
+    public ServerException(String message) {
+        super(message);
+    }
+}
