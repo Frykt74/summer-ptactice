47c36e773ae5b3a9a9dc9359ea516a2ac76a8894
frykt <coch.frykt@gmail.com>
2024-08-29 14:36:12 +0300

    add exception

--- src/main/java/org/example/fishingconfig47/exceptions/NotEnoughBudgetException.java
+++ src/main/java/org/example/fishingconfig47/exceptions/NotEnoughBudgetException.java
@@ -1,6 +1,6 @@
 package org.example.fishingconfig47.exceptions;
 
-public class NotEnoughBudgetException extends RuntimeException {
+public class NotEnoughBudgetException extends ValidationException {
     public NotEnoughBudgetException(String message) {
         super(message);
     }
