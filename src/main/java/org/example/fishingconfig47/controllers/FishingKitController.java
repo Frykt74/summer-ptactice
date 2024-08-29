dae18a6359914f9b231dc6a37f65ae55cdbc36c3
frykt <coch.frykt@gmail.com>
2024-08-29 14:41:26 +0300

    add budget selection

--- src/main/java/org/example/fishingconfig47/controllers/FishingKitController.java
+++ src/main/java/org/example/fishingconfig47/controllers/FishingKitController.java
@@ -1,7 +1,7 @@
 package org.example.fishingconfig47.controllers;
 
 import org.example.fishingconfig47.dtos.FishingKitDto;
-import org.example.fishingconfig47.entities.FishingKit;
+import org.example.fishingconfig47.services.BudgetKitService;
 import org.example.fishingconfig47.services.FishingKitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,22 +15,30 @@ import java.util.List;
 @RequestMapping("/fishing-kits")
 public class FishingKitController {
     private final FishingKitService fishingKitService;
+    private final BudgetKitService budgetKitService;
 
     @Autowired
-    public FishingKitController(FishingKitService fishingKitService) {
+    public FishingKitController(FishingKitService fishingKitService,
+                                BudgetKitService budgetKitService) {
         this.fishingKitService = fishingKitService;
+        this.budgetKitService = budgetKitService;
     }
 
     @PostMapping
     public ResponseEntity<FishingKitDto> createFishingKit(@RequestBody FishingKitDto fishingKitDto) {
-
         FishingKitDto createdFishingKit = fishingKitService.createFishingKit(fishingKitDto);
         return new ResponseEntity<>(createdFishingKit, HttpStatus.CREATED);
     }
 
     @GetMapping("/analyze")
-    public ResponseEntity<List<FishingKit>> analyzeFishingKits() {
-        List<FishingKit> fishingKits = fishingKitService.analyzeFishingKits();
+    public ResponseEntity<List<FishingKitDto>> analyzeFishingKits() {
+        List<FishingKitDto> fishingKits = fishingKitService.analyzeFishingKits();
         return new ResponseEntity<>(fishingKits, HttpStatus.OK);
+    }
+
+    @PostMapping("/budget")
+    public ResponseEntity<FishingKitDto> budgetFishingKits(@RequestBody Integer budget) {
+        FishingKitDto budgetFishingKit = budgetKitService.selectKitByBudget(budget);
+        return new ResponseEntity<>(budgetFishingKit, HttpStatus.OK);
     }
 }
--- src/main/java/org/example/fishingconfig47/services/impl/BudgetKitServiceImpl.java
+++ src/main/java/org/example/fishingconfig47/services/impl/BudgetKitServiceImpl.java
@@ -1,15 +1,15 @@
 package org.example.fishingconfig47.services.impl;
 
+import org.example.fishingconfig47.dtos.FishingKitDto;
 import org.example.fishingconfig47.entities.*;
 import org.example.fishingconfig47.exceptions.NotEnoughBudgetException;
-import org.example.fishingconfig47.exceptions.ServerException;
-import org.example.fishingconfig47.repositories.LineRepository;
-import org.example.fishingconfig47.repositories.LureRepository;
-import org.example.fishingconfig47.repositories.ReelRepository;
-import org.example.fishingconfig47.repositories.RodRepository;
+import org.example.fishingconfig47.repositories.*;
 import org.example.fishingconfig47.services.BudgetKitService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BudgetKitServiceImpl implements BudgetKitService {
@@ -18,21 +18,71 @@ public class BudgetKitServiceImpl implements BudgetKitService {
     private final ReelRepository reelRepository;
     private final LineRepository lineRepository;
     private final LureRepository lureRepository;
+    private final RodReelRepository rodReelRepository;
+    private final FishingKitRepository fishingKitRepository;
+
+    private final ModelMapper modelMapper;
 
     @Autowired
     public BudgetKitServiceImpl(RodRepository rodRepository,
                                 ReelRepository reelRepository,
                                 LineRepository lineRepository,
-                                LureRepository lureRepository) {
+                                LureRepository lureRepository,
+                                RodReelRepository rodReelRepository,
+                                FishingKitRepository fishingKitRepository,
+                                ModelMapper modelMapper) {
 
         this.rodRepository = rodRepository;
         this.reelRepository = reelRepository;
         this.lineRepository = lineRepository;
         this.lureRepository = lureRepository;
+        this.rodReelRepository = rodReelRepository;
+        this.fishingKitRepository = fishingKitRepository;
+
+        this.modelMapper = modelMapper;
     }
 
     @Override
-    public FishingKit selectKitByBudget(Double budget) {
+    public FishingKitDto selectKitByBudget(Integer budget) {
+        int rodBudget = (int) (budget * 0.58);
+        int reelBudget = (int) (budget * 0.31);
+        int lineBudget = (int) (budget * 0.05);
+        int lureBudget = (int) (budget * 0.06);
 
+        List<Rod> rods = rodRepository.findTop4ByPriceLessThanEqualOrderByPriceDesc(rodBudget);
+        List<Reel> reels = reelRepository.findTop4ByPriceLessThanEqualOrderByPriceDesc(reelBudget);
+        List<Line> lines = lineRepository.findTop4ByPriceLessThanEqualOrderByPriceDesc(lineBudget);
+        List<Lure> lures = lureRepository.findTop4ByPriceLessThanEqualOrderByPriceDesc(lureBudget);
+
+        if (rods.isEmpty() || reels.isEmpty() || lines.isEmpty() || lures.isEmpty()) {
+            throw new NotEnoughBudgetException("Недостаточно средств для создания набора");
+        }
+
+        for (Rod rod : rods) {
+            for (Reel reel : reels) {
+                if (!rodReelRepository.existsRodAndReel(rod, reel)) {
+                    continue;
+                }
+
+                for (Line line : lines) {
+                    if (line.getTestWidth() > reel.getMaxDrag()) {
+                        continue;
+                    }
+
+                    for (Lure lure : lures) {
+                        if (lure.getWeight() < rod.getLureWeightMin() || lure.getWeight() > rod.getLureWeightMax()) {
+                            continue;
+                        }
+
+                        FishingKit budgetFishingKit = new FishingKit("New kit", rod, reel, line, lure, 0f, 0);
+
+                        FishingKit savedFishingKit = fishingKitRepository.save(budgetFishingKit);
+                        return modelMapper.map(savedFishingKit, FishingKitDto.class);
+                    }
+                }
+            }
+        }
+
+        throw new NotEnoughBudgetException("Не удалось подобрать подходящий набор для данного бюджета.");
     }
 }
