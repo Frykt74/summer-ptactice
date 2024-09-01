5dd7590ad8e338753dbcdb88458a01cd95b52905
frykt <coch.frykt@gmail.com>
2024-09-01 21:25:45 +0300

    add delete repository

--- src/main/java/org/example/fishingconfig47/repositories/FishingKitRepository.java
+++ src/main/java/org/example/fishingconfig47/repositories/FishingKitRepository.java
@@ -2,6 +2,7 @@ package org.example.fishingconfig47.repositories;
 
 import org.example.fishingconfig47.entities.FishingKit;
 import org.example.fishingconfig47.repositories.generics.CreateRepository;
+import org.example.fishingconfig47.repositories.generics.DeleteRepository;
 import org.example.fishingconfig47.repositories.generics.ReadRepository;
 import org.example.fishingconfig47.repositories.generics.UpdateRepository;
 import org.springframework.data.repository.NoRepositoryBean;
@@ -12,7 +13,8 @@ import java.util.List;
 public interface FishingKitRepository extends
         ReadRepository<FishingKit, Integer>,
         CreateRepository<FishingKit, Integer>,
-        UpdateRepository<FishingKit, Integer> {
+        UpdateRepository<FishingKit, Integer>,
+        DeleteRepository<FishingKit, Integer> {
     List<FishingKit> findKitsByRodNameAndReelName(String rodName, String reelName);
 
     List<FishingKit> findFishingKitsByLureNameAndLureColor(String name, String color);
